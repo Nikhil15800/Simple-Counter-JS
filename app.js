0ea9b1744d2127e667f9303939a6e684246c3fa1
Nikhil Khandelwal <55945199+Nikhil15800@users.noreply.github.com>
2022-08-30 14:41:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// let  = 0;
+
+let show = 0;
+
+ function dec(){
+   show = show-1;
+   document.getElementById('show').innerHTML = show;
+}
+function inc(){
+   
+   show = show+1;
+   document.getElementById('show').innerHTML = show;
+   // continue;
+   
+   // let show = document.getElementsByClassName('show').innerHtml = btn2;
+   // document.write(show);
+}
+// inc();
